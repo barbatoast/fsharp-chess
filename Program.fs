746f82d86f789dfa19085291bc10bd2f7d80fa04
barbatoast <christhomas718@gmail.com>
2025-05-10 11:11:35 -0400

    Add pawn legal move check

--- Program.fs
+++ Program.fs
@@ -44,6 +44,40 @@ let board : char option[,] = Array2D.create boardSize boardSize None
 
 let mkPiece color pieceType = Some { pieceType = pieceType; color = color; hasMoved = false }
 
+// Optional: track selected tile
+let mutable selectedX = -1
+let mutable selectedY = -1
+
+let isLegalPawnMove (board: Board) (piece: Piece) (fromX, fromY) (toX, toY) =
+    let direction = match piece.color with White -> -1 | Black -> 1
+    let startRank = match piece.color with White -> 6 | Black -> 1
+
+    let dx = toX - fromX
+    let dy = toY - fromY
+
+    let target = board.[toX, toY]
+
+    match dx, dy with
+    // Forward move by 1 into empty square
+    | 0, d when d = direction && target.IsNone -> true
+    // Forward move by 2 from start rank, both tiles empty
+    | 0, d when d = 2 * direction && fromY = startRank ->
+        let intermediateY = fromY + direction
+        board.[toX, intermediateY].IsNone && target.IsNone
+    // Diagonal capture
+    | dx, d when abs dx = 1 && d = direction ->
+        match target with
+        | Some t when t.color <> piece.color -> true
+        | _ -> false
+    // Anything else is illegal
+    | _ -> false
+
+let isLegalMove (board: Board) (piece: Piece) (fromX, fromY) (toX, toY) =
+    match piece.pieceType with
+    | Pawn -> isLegalPawnMove board piece (fromX, fromY) (toX, toY)
+    | _ -> true // For now allow all other pieces to move anywhere
+
+
 let initializeBoard () =
     for i in 0 .. 7 do
         gameState.board.[i, 1] <- mkPiece Black Pawn
@@ -65,11 +99,6 @@ let drawBoard () =
             | Some (selX, selY) when selX = x && selY = y ->
                 Raylib.DrawRectangleLinesEx(Rectangle(float32 (x * tileSize), float32 (y * tileSize), float32 tileSize, float32 tileSize), 4.0f, highlightColor)
             | _ -> ()
-
-
-// Optional: track selected tile
-let mutable selectedX = -1
-let mutable selectedY = -1
 
 let pieceToChar (piece: Piece) : char =
     let c =
@@ -101,25 +130,27 @@ let handleMouseClick () =
         let x = int mousePos.X / tileSize
         let y = int mousePos.Y / tileSize
 
-        if isInsideBoard x y then
-            match gameState.selected with
-            | None ->
-                // Select if there's a piece of correct color
-                match gameState.board.[x, y] with
-                | Some piece when (piece.color = (if gameState.whiteTurn then White else Black)) ->
-                    gameState <- { gameState with selected = Some (x, y) }
-                | _ -> ()
+        match gameState.selected with
+        | None ->
+            match gameState.board.[x, y] with
+            | Some piece when piece.color = (if gameState.whiteTurn then White else Black) ->
+                gameState <- { gameState with selected = Some (x, y) }
+            | _ -> ()
 
-            | Some (fromX, fromY) ->
-                // Move piece
-                let pieceOpt = gameState.board.[fromX, fromY]
-                match pieceOpt with
-                | Some piece ->
-                    gameState.board.[x, y] <- Some { piece with hasMoved = true }
-                    gameState.board.[fromX, fromY] <- None
-                    gameState <- { gameState with selected = None; whiteTurn = not gameState.whiteTurn }
-                | None ->
-                    gameState <- { gameState with selected = None }
+        | Some (fromX, fromY) ->
+            match gameState.board.[fromX, fromY] with
+            | Some piece when isLegalMove gameState.board piece (fromX, fromY) (x, y) ->
+                let target = gameState.board.[x, y]
+                let isCapture =
+                    match target with
+                    | Some t -> t.color <> piece.color
+                    | None -> false
+
+                gameState.board.[x, y] <- Some { piece with hasMoved = true }
+                gameState.board.[fromX, fromY] <- None
+                gameState <- { gameState with selected = None; whiteTurn = not gameState.whiteTurn }
+            | _ ->
+                gameState <- { gameState with selected = None }
 
 [<EntryPoint>]
 let main _ =
