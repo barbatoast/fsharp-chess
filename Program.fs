585e5fce0dc2a16e101e166d83b0236ff3afabfb
barbatoast <christhomas718@gmail.com>
2025-05-10 11:05:45 -0400

    Add game state

--- Program.fs
+++ Program.fs
@@ -1,9 +1,41 @@
 ﻿open Raylib_cs
 
-let screenWidth = 800
-let screenHeight = 800
-let tileSize = 100
+// -- Types --
+
+type Color = White | Black
+
+type PieceType =
+    | Pawn
+    | Knight
+    | Bishop
+    | Rook
+    | Queen
+    | King
+
+type Piece = {
+    pieceType: PieceType
+    color: Color
+    hasMoved: bool
+}
+
+type Board = Piece option [,]
+
+type GameState = {
+    board: Board
+    whiteTurn: bool
+    selected: (int * int) option
+}
+
 let boardSize = 8
+let tileSize = 100
+let screenWidth = boardSize * tileSize
+let screenHeight = boardSize * tileSize
+
+let mutable gameState = {
+    board = Array2D.create boardSize boardSize None
+    whiteTurn = true
+    selected = None
+}
 
 let lightColor = Color(0xee, 0xee, 0xd2, 255)
 let darkColor = Color(0x76, 0x96, 0x56, 255)
@@ -12,19 +44,17 @@ let highlightColor = Color.RED
 // 2D board: Some char = a piece, None = empty
 let board : char option[,] = Array2D.create boardSize boardSize None
 
-// Initialize a few pieces
+let mkPiece color pieceType = Some { pieceType = pieceType; color = color; hasMoved = false }
+
 let initializeBoard () =
     for i in 0 .. 7 do
-        board.[i, 1] <- Some '♟' // Black pawns
-        board.[i, 6] <- Some '♙' // White pawns
-    board.[0, 0] <- Some '♜'; board.[7, 0] <- Some '♜' // Black rooks
-    board.[0, 7] <- Some '♖'; board.[7, 7] <- Some '♖' // White rooks
-    board.[3, 0] <- Some '♛'
-    board.[3, 7] <- Some '♕'
+        gameState.board.[i, 1] <- mkPiece Black Pawn
+        gameState.board.[i, 6] <- mkPiece White Pawn
 
-// Optional: track selected tile
-let mutable selectedX = -1
-let mutable selectedY = -1
+    let backRow = [ Rook; Knight; Bishop; Queen; King; Bishop; Knight; Rook ]
+    for i in 0 .. 7 do
+        gameState.board.[i, 0] <- mkPiece Black backRow.[i]
+        gameState.board.[i, 7] <- mkPiece White backRow.[i]
 
 let drawBoard () =
     for x in 0 .. boardSize - 1 do
@@ -33,17 +63,65 @@ let drawBoard () =
             let color = if isLight then lightColor else darkColor
             Raylib.DrawRectangle(x * tileSize, y * tileSize, tileSize, tileSize, color)
 
-            // Highlight selected tile
-            if x = selectedX && y = selectedY then
-                Raylib.DrawRectangleLinesEx(Rectangle(float32 (x * tileSize), float32 (y * tileSize), float32 tileSize, float32 tileSize), 3.0f, highlightColor)
+            match gameState.selected with
+            | Some (selX, selY) when selX = x && selY = y ->
+                Raylib.DrawRectangleLinesEx(Rectangle(float32 (x * tileSize), float32 (y * tileSize), float32 tileSize, float32 tileSize), 4.0f, highlightColor)
+            | _ -> ()
+
+
+// Optional: track selected tile
+let mutable selectedX = -1
+let mutable selectedY = -1
+
+let pieceToChar (piece: Piece) : char =
+    let c =
+        match piece.pieceType with
+        | Pawn -> 'P'
+        | Knight -> 'N'
+        | Bishop -> 'B'
+        | Rook -> 'R'
+        | Queen -> 'Q'
+        | King -> 'K'
+    match piece.color with
+    | White -> c
+    | Black -> System.Char.ToLowerInvariant(c)
 
 let drawPieces () =
     for x in 0 .. boardSize - 1 do
         for y in 0 .. boardSize - 1 do
-            match board.[x, y] with
+            match gameState.board.[x, y] with
             | Some piece ->
-                Raylib.DrawText(piece.ToString(), x * tileSize + 35, y * tileSize + 25, 40, Color.BLACK)
+                let text = pieceToChar piece |> string
+                Raylib.DrawText(text, x * tileSize + 35, y * tileSize + 25, 40, Color.BLACK)
             | None -> ()
+let isInsideBoard (x: int) (y: int) =
+    x >= 0 && x < boardSize && y >= 0 && y < boardSize
+
+let handleMouseClick () =
+    if Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON) |> CBool.op_Implicit then
+        let mousePos = Raylib.GetMousePosition()
+        let x = int mousePos.X / tileSize
+        let y = int mousePos.Y / tileSize
+
+        if isInsideBoard x y then
+            match gameState.selected with
+            | None ->
+                // Select if there's a piece of correct color
+                match gameState.board.[x, y] with
+                | Some piece when (piece.color = (if gameState.whiteTurn then White else Black)) ->
+                    gameState <- { gameState with selected = Some (x, y) }
+                | _ -> ()
+
+            | Some (fromX, fromY) ->
+                // Move piece
+                let pieceOpt = gameState.board.[fromX, fromY]
+                match pieceOpt with
+                | Some piece ->
+                    gameState.board.[x, y] <- Some { piece with hasMoved = true }
+                    gameState.board.[fromX, fromY] <- None
+                    gameState <- { gameState with selected = None; whiteTurn = not gameState.whiteTurn }
+                | None ->
+                    gameState <- { gameState with selected = None }
 
 let checkMouseClick () =
     if CBool.op_Implicit(Raylib.IsMouseButtonPressed MouseButton.MOUSE_LEFT_BUTTON) then
@@ -57,15 +135,15 @@ let checkMouseClick () =
 
 [<EntryPoint>]
 let main _ =
-    Raylib.InitWindow(screenWidth, screenHeight, "F# Chessboard with Pieces")
+    Raylib.InitWindow(screenWidth, screenHeight, "F# Chess - World State")
     Raylib.SetTargetFPS 60
     initializeBoard()
 
-    while not (CBool.op_Implicit(Raylib.WindowShouldClose())) do
-        checkMouseClick()
+    while not (Raylib.WindowShouldClose() |> CBool.op_Implicit) do
+        handleMouseClick()
 
         Raylib.BeginDrawing()
-        Raylib.ClearBackground Color.RAYWHITE
+        Raylib.ClearBackground(Color.RAYWHITE)
 
         drawBoard()
         drawPieces()
